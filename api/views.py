8b105bb43c237f352c71ccc24a2df1fdcc3b7cec
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-07-08 10:06:03 -0600

    Adds basic setup, also adds beginnings of api/views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from ./models.py import Food
+import json
 
 # Create your views here.
+def food_index(request):
+    return JsonResponse(Food.objects.all())
