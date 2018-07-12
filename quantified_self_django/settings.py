c540f4c93979c40adcdfb24d3c14db5686ac4644
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-07-12 00:21:51 -0600

    Installs corsheaders, adds to installed and middleware lists

--- quantified_self_django/settings.py
+++ quantified_self_django/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
