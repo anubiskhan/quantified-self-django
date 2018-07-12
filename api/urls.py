401d9502d80c9d6bc80c2ffd4f6d3e1a00e70d71
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-07-11 18:13:41 -0600

    Adds happy pathing for food crud

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import FoodsView
 
 
 urlpatterns = [
-    path('api/v1/foods/', views.get_foods, name='get_foods'),
-    path('api/v1/foods', views.post_food, name='post_food'),
+    path('api/v1/foods/', FoodsView.as_view({'get': 'list', 'post': 'create'})),
+    path('api/v1/foods/<food_id>', FoodsView.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'update', 'delete': 'destroy'})),
 ]
--- api/views.py
+++ api/views.py
@@ -1,20 +1,43 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 from .models import Food
 from .serializers import FoodSerializer
+from django.shortcuts import get_object_or_404
 
-@api_view(['GET'])
-def get_foods(request):
-    # get all foods
-    foods = Food.objects.all()
-    serializer = FoodSerializer(foods, many=True)
-    return Response(serializer.data)
-@api_view(['POST'])
-def post_food(request):
-    # post a new food
-    name = request.data["food"]["name"]
-    calories = request.data["food"]["calories"]
-    food = Food.objects.create(name = name, calories = calories)
-    serializer = FoodSerializer(food, many=False)
-    return Response(serializer.data)
+class FoodsView(viewsets.ViewSet):
+    def list(self, request):
+        # get all foods
+        foods = Food.objects.all()
+        serializer = FoodSerializer(foods, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        # post a new food
+        name = request.data["food"]["name"]
+        calories = request.data["food"]["calories"]
+        food = Food.objects.create(name = name, calories = calories)
+        serializer = FoodSerializer(food, many=False)
+        return Response(serializer.data)
+
+    def retrieve(self, request, food_id):
+        # get a single food
+        food = Food.objects.get(id=food_id)
+        serializer = FoodSerializer(food, many=False)
+        return Response(serializer.data)
+
+    def update(self, request, food_id):
+        # update a single food
+        name = request.data["food"]["name"]
+        calories = request.data["food"]["calories"]
+        food = Food.objects.get(id=food_id)
+        food.name = name
+        food.calories = calories
+        food.save()
+        serializer = FoodSerializer(food, many=False)
+        return Response(serializer.data)
+
+    def destroy(self, request, food_id):
+        food = Food.objects.get(id=food_id)
+        food.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
