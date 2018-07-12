ab1f1b01f504e16518b03fcd38de4c42c7c18846
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-07-11 18:20:35 -0600

    Adds meals serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from .models import Food
+from .models import Food, Meal
 
 
 class FoodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Food
         fields = ('id', 'name', 'calories')
+
+class MealSerializer(serializers.ModelSerializer):
+    foods = FoodSerializer(many=True)
+
+    class Meta:
+        model = Meal
+        fields = ('id', 'name', 'foods')
