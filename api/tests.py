2c1daeec515fbca8f67f25175d40472cf7579a27
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-07-23 19:08:00 -0600

    Adds test for meals endpoint

--- api/tests.py
+++ api/tests.py
@@ -7,7 +7,7 @@ import json
 # Create your tests here.
 client = Client()
 
-class ApiTest(TestCase):
+class FoodApiTest(TestCase):
     def setUp(self):
         Food.objects.create(name='Garsh', calories=101)
         Food.objects.create(name='Darsh', calories=123)
@@ -19,3 +19,14 @@ class ApiTest(TestCase):
         self.assertEqual(json.loads(response.content)[0]['calories'], 101)
         self.assertEqual(json.loads(response.content)[1]['name'], 'Darsh')
         self.assertEqual(json.loads(response.content)[1]['calories'], 123)
+
+class MealApiTest(TestCase):
+    def setUp(self):
+        Meal.objects.create(name='Breakfast')
+        Meal.objects.create(name='Snack')
+
+    def test_meal_list_endpoint(self):
+        response = client.get('/api/v1/meals/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content)[0]['name'], 'Breakfast')
+        self.assertEqual(json.loads(response.content)[1]['name'], 'Snack')
